54b427930195fe8c8f62f5d626b4e490095f66fb
hiran <hiran@homeserver.indernet>
2023-07-07 18:04:21 +0200

    Added feedback for MrGimlet

--- src/main/java/oolite/starter/MainFrame.java
+++ src/main/java/oolite/starter/MainFrame.java
@@ -42,7 +42,7 @@ public class MainFrame extends javax.swing.JFrame {
         if (confFile.exists()) {
             configuration = new Configuration(confFile);
         } else {
-            String msg = String.format("<html><p>Heho, Kid! You've got a problem here that is technical, not financial.</p><p>The configuration file %s was not found.</p><p>I’m a busy frog, I can’t stay here all day to watch you get it right. So let's use defaults.</p></html>", confFile.getAbsolutePath());
+            String msg = String.format("<html><p>Heho, Kid! You've got a problem here that is technical, not financial.</p><p>The configuration file %s was not found.</p><p>I’m a busy frog, I can’t stay here all day to watching you poke buttons. So let's use defaults.</p></html>", confFile.getAbsolutePath());
             log.warn(msg);
             
             MrGimlet.showMessage(null, msg);
@@ -151,10 +151,10 @@ public class MainFrame extends javax.swing.JFrame {
                     
                     StringBuilder message = new StringBuilder("<html>");
                     message.append("<p>I see a lot of blanks on this here board... Kid, you gotta do something about it.</p>");
-                    message.append("<p>Have at least one active Oolite version. You need to. In fact, everyone must have it!<br/>");
+                    message.append("<p>Have at least one active Oolite version. You need one. It's pretty much compulsory.<br/>");
                     message.append("Hit the Add button and fill in the form, at least once to add Oolite versions.<br/>");
-                    message.append("Do not forget to select one of them and hit the Activate button.</p>");
-                    message.append("<p>Only then try to juggle OXPs or start the game. Right... Ah, remember to gently touch<br/>the Save button before you leave the area.</p>");
+                    message.append("Don't forget to select one of them, then hit the Activate button.</p>");
+                    message.append("<p>Then you can juggle OXPs or start the game. And maybe touch the Save button once in a while.</p>");
                     message.append("</html>");
 
                     MrGimlet.showMessage(mf, message.toString());
