be30b130b87b23d6d2b36f0859f0156c0322311a
cube <cube@oolite.space>
2024-09-02 22:59:32 +0200

    Allow to view flavor content (show button)

--- src/main/java/oolite/starter/Oolite.java
+++ src/main/java/oolite/starter/Oolite.java
@@ -1687,7 +1687,7 @@ public class Oolite implements PropertyChangeListener {
         List<Command> result = new ArrayList<>();
 
         TreeMap<String, String> enabledAddons = prepareEnabledAddonsList(target);
-
+        // check what needs to be uninstalled
         result.addAll(prepareDisableCommands(enabledAddons, expansions));
 
         // now INSTALL what may be MISSING
@@ -1706,11 +1706,12 @@ public class Oolite implements PropertyChangeListener {
                 expansion = expansionMap.get(i2);
             }
             if (expansion == null) {
+                // we are trying to install something that is not part of the catalog?
                 Expansion e = new Expansion();
                 e.setOolite(this);
                 e.setTitle(i);
                 e.setDownloadUrl(enabledAddons.get(i));
-                result.add(new Command(Command.Action.UNKNOWN, e));
+                result.add(new Command(Command.Action.INSTALL, e));
                 log.info("Trying expansionset download {}", e);
             } else if (expansion.isLocal() && expansion.isEnabled()) {
                 // already here - do nothing
@@ -2645,6 +2646,7 @@ public class Oolite implements PropertyChangeListener {
         }
         
         tcpserver = new TCPServer();
+        
         try {
             MQTTAdapter ma = new MQTTAdapter();
             ma.init(tcpserver, configuration.getActiveInstallation().getMqtt());
